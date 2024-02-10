fd625aa8d91d2df5063b932151b370be3197e5f5
DimaM <38975569+DimaMdv@users.noreply.github.com>
2024-02-10 12:28:57 -0600

    Added central illinois plants

--- my-first-garden/src/plantData.js
+++ my-first-garden/src/plantData.js
@@ -6,7 +6,7 @@ export const NorthernIllinoisplants = [
         size:[1],
         info:["Soak in water 24 hours before planting","Beets are not the best at competing with weeds especially when younger, regular weeding and mulching when the leaves get tall enough is recommended"],
         sun:[0],
-    }
+    },
     {
         plantName: "Turnip",
         zones: ["2-9"],
@@ -14,7 +14,7 @@ export const NorthernIllinoisplants = [
         size:[2],
         sun:[0],
         info: ["Root maggots are the most prevalent problem, use diatomaceous earth around the root to dehydrate and kill them"],
-    }
+    },
     {
         plantName: "Peas",
         zones: ["2-11"],
@@ -22,7 +22,7 @@ export const NorthernIllinoisplants = [
         size:[1],
         sun:[0],
         info: ["Root-rot disease is the most prevalent problem with peas, which can be resolved with properly drained soil."],
-    }
+    },
     {
         plantName: "Winter Squash",
         zones: ["3-9"],
@@ -32,3 +32,39 @@ export const NorthernIllinoisplants = [
         info: ["The biggest problem is cucumber beetles, they can be deterred with neem oil extract, a natural pesticide against them."],
     }
   ]
+
+export const CentralIllinoisplants = [
+{
+    plantName: "Mustard",
+    zones: ["2-11"],
+    dates: ["04/01","09/31"],
+    size:[4],
+    info:["Soak befor planting.", "Regular weeding required."],
+    sun:[1]
+},
+{
+    plantName: "Carrots",
+    zones: ["4-8"],
+    dates: ["04/10","07/15"],
+    size:[0.25],
+    info:["Precise water maintenance required.", "Be aware of rodens."],
+    sun:[1]
+},
+{
+    plantName: "Radish",
+    zones: ["2-11"],
+    dates: ["04/5","07/1"],
+    size:[1],
+    info:["Works well in between slow-maturing vegetables.", "Root maggots are the most prevalent problem."],
+    sun:[1]
+},
+{
+    plantName: "Pumpkin",
+    zones: ["3-9"],
+    dates: ["03/20","06/10"],
+    size:[72],
+    info:["Works well in between slow-maturing vegetables.", "One of the biggest problems for pumpkins is powdery mildew."],
+    sun:[1]
+}
+
+]
