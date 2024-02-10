cdea7971fda185cea0083e17e05cbb9dd6b05b30
DimaM <38975569+DimaMdv@users.noreply.github.com>
2024-02-10 12:53:18 -0600

    Fixed formationg for consistency

--- my-first-garden/src/plantData.js
+++ my-first-garden/src/plantData.js
@@ -2,26 +2,34 @@ export const NorthernIllinoisplants = [
     {
         plantName: "Beets",
         size:[1],
-        info:["Grows in zones 2–11, for zone 5 plant between April 24 and July 30.","Plant 1 inch apart, Soak in water 24 hours before planting; thin seedlings to 2 inches apart.","Beets are not the best at competing with weeds especially when younger, regular weeding and mulching when the leaves get tall enough is recommended"],
         sun:[0],
+        info:["Grows in zones 2–11, for zone 5 plant between April 24 and July 30.",
+        "Plant 1 inch apart, Soak in water 24 hours before planting; thin seedlings to 2 inches apart.",
+        "Beets are not the best at competing with weeds especially when younger, regular weeding and mulching when the leaves get tall enough is recommended"]
     },
     {
         plantName: "Turnip",
         size:[2],
         sun:[0],
-        info: ["Grows in Zones 2-9, for zone 5 plant between May 15 and Aug. 5 for a fall harvest","Plant 2 inches apart","Root maggots are the most prevalent problem, use diatomaceous earth around the root to dehydrate and kill them"],
+        info: ["Grows in Zones 2-9, for zone 5 plant between May 15 and Aug. 5 for a fall harvest",
+        "Plant 2 inches apart",
+        "Root maggots are the most prevalent problem, use diatomaceous earth around the root to dehydrate and kill them"],
     },
     {
         plantName: "Peas",
         size:[1],
         sun:[0],
-        info: ["Grows in Zones 2-11, for zone 5 plant between April 24 and May 15","Plant 1 inch apart", "Root-rot disease is the most prevalent problem with peas, which can be resolved with properly drained soil."],
+        info: ["Grows in Zones 2-11, for zone 5 plant between April 24 and May 15",
+        "Plant 1 inch apart", 
+        "Root-rot disease is the most prevalent problem with peas, which can be resolved with properly drained soil."],
     },
     {
         plantName: "Winter Squash",
         size:[72],
         sun:[1],
-        info: ["rows in Zones 3-9, for zone 5 plant between June 5th and 15th","Plant 4 seeds per hill and thin to the 2 strongest seedlings", "The biggest problem is cucumber beetles, they can be deterred with neem oil extract, a natural pesticide against them."],
+        info: ["rows in Zones 3-9, for zone 5 plant between June 5th and 15th",
+        "Plant 4 seeds per hill and thin to the 2 strongest seedlings", 
+        "The biggest problem is cucumber beetles, they can be deterred with neem oil extract, a natural pesticide against them."],
     }
   ]
 
@@ -29,26 +37,34 @@ export const NorthernIllinoisplants = [
     {
         plantName: "Mustard",
         size:[4],
-        info:["Grows in Zones 2-11, for zone 6 plant between April 1 to 30 and/or July 1 to August 31.", "Soak befor planting.", "Regular weeding required."],
-        sun:[1]
+        sun:[1],
+        info:["Grows in Zones 2-11, for zone 6 plant between April 1 to 30 and/or July 1 to August 31.", 
+        "Soak befor planting.", 
+        "Regular weeding required."]
     },
     {
         plantName: "Carrots",
         size:[0.25],
-        info:["Grows in Zones 4-8, for zone 6 plant from April 10 to July 15 for a fall harvest.","Precise water maintenance required.", "Be aware of rodens."],
-        sun:[1]
+        sun:[1],
+        info:["Grows in Zones 4-8, for zone 6 plant from April 10 to July 15 for a fall harvest.",
+        "Precise water maintenance required.",
+        "Be aware of rodens."]
     },
     {
         plantName: "Radish",
         size:[1],
-        info:["Grows in Zones 2-11, for zone 6 plant between April 5 and June 1, and/or Aug. 1 to 30", "Works well in between slow-maturing vegetables.", "Root maggots are the most prevalent problem."],
-        sun:[1]
+        sun:[1],
+        info:["Grows in Zones 2-11, for zone 6 plant between April 5 and June 1, and/or Aug. 1 to 30",
+        "Works well in between slow-maturing vegetables.",
+        "Root maggots are the most prevalent problem."]
     },
     {
         plantName: "Pumpkin",
         size:[72],
-        info:["Grows in Zones 3-9, for zone 6 plant between May 20 and June 10", "Works well in between slow-maturing vegetables.", "One of the biggest problems for pumpkins is powdery mildew."],
-        sun:[1]
+        sun:[1],
+        info:["Grows in Zones 3-9, for zone 6 plant between May 20 and June 10",
+        "Works well in between slow-maturing vegetables.",
+        "One of the biggest problems for pumpkins is powdery mildew."]
     }
   ]
 
@@ -56,10 +72,10 @@ export const SouthernIllinoisplants = [
     {
         plantName: "Parsnip",
         size:[3],
+        sun:[0],
         info:["Grows in Zones 2-9, for zone 7 plant from April 15 to July 7 as a fall crop",
         "Plant 3 inches apart”",
-        "Low soil fertility is the only significant problem for parsnips due to a lack of insects and diseases that target them. This can be avoided by side-fertilizing in July."],
-        sun:[0],
+        "Low soil fertility is the only significant problem for parsnips due to a lack of insects and diseases that target them. This can be avoided by side-fertilizing in July."]
     },
     {
         plantName: "Onion",
