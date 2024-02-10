958af26c9a1a97b18bedf62414fe19c231b41ee4
itsasam1 <mikeeaglesam@gmail.com>
2024-02-10 06:40:49 -0600

    updated the climate_weather.api

--- api/climate_weather.java
+++ api/climate_weather.java
@@ -1,3 +1,34 @@
 import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReador;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+
+public class climate {
+    public static void main(String[] args) {
+        try {
+            private String rain_url = (
+                "http://climateapi.scottpinkelman.com/api/v1/location/" +
+                "[" + lattitude + "]" +
+                "/" +
+                "[" + longitude + "]" +
+                "/json?api_key=" +
+                rain_api_key()
+                )
+
+            URL url = new URL(rain_url)
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Accept", "application/json");
+            if (conn.getResponseCode() != 200) {
+                throw new RuntimeException("Failed : HTTP Error code : "
+                        + conn.getResponseCode());
+            }
+            
+            conn.disconnect();
+
+        } catch (Exception e) {
+            System.out.println("Exception in NetClientGet:- " + e);
+        }
+    }
+}
