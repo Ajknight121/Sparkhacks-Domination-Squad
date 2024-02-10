4c0a74eabc4e55e9ac885d23b766e8a5ef38e870
DimaM <38975569+DimaMdv@users.noreply.github.com>
2024-02-10 11:01:25 -0600

    Pot the temp API key

--- api/geo_code.java
+++ api/geo_code.java
@@ -4,22 +4,21 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class geo_code{
-    public static void main(String[] args, String yesterday_time, String today_time, double lattitude, double longitude, int zipCode) {
+    public static void main(String[] args, int zipCode) {
         try {
             String rain_url = (
                 "https://maps.googleapis.com/maps/api/geocode/json?postal_code=" +
                 String.valueOf(zipCode) +
                 "CA&key=" 
-                +" "
+                + "ae62075c8ac9cdc7770d0006dce529c629095ea"
                 );
 
-            URL url = new URL(rain_url)
+            URL url = new URL(rain_url);
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setRequestMethod("GET");
             conn.setRequestProperty("Accept", "application/json");
             if (conn.getResponseCode() != 200) {
-                throw new RuntimeException("Failed : HTTP Error code : "
-                        + conn.getResponseCode());
+                throw new RuntimeException("Failed : HTTP Error code : "+ conn.getResponseCode());
             }
             
             conn.disconnect();
