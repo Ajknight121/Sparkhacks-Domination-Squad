895125d9bceabf86f357740de251d83a40ef2d41
itsasam1 <mikeeaglesam@gmail.com>
2024-02-10 12:09:55 -0600

    added stuff

---
+++ my-first-garden/src/plantData.js
@@ -0,0 +1,12 @@
+export const NorthernIllinoisplants = [
+    {
+      plantName: "Beets",
+      info:["takes up 3ft", "requires 1gal of water per day"],
+      pests: ["caterpillers", "Rodents"],
+    },
+    {
+        plantName: "Beets",
+        info:["takes up 3ft", "requires 1gal of water per day"],
+        pests: ["caterpillers", "Rodents"],
+    }
+  ]
