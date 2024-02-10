418eb507620f06ed6ff254f985ea2cb4b175238d
Ajknight121 <aknig2@uic.edu>
2024-02-10 00:33:03 -0600

    Added tailwindcss and full cover background img

--- my-first-garden/src/App.jsx
+++ my-first-garden/src/App.jsx
@@ -1,33 +1,15 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import spaceBackground from "./images/space-background.gif"
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [step, setStep] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='container w-full h-screen relative'>
+        <img src={spaceBackground} className="background-img absolute top-0 left-0 w-full h-full object-cover"/>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
