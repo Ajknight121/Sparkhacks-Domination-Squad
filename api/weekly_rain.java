591b3a7f3868f08d0d65622a54c73b28392f4e6e
itsasam1 <mikeeaglesam@gmail.com>
2024-02-10 06:30:32 -0600

    changed weekly_rain.java but I do not know how functions work

--- api/weekly_rain.java
+++ api/weekly_rain.java
@@ -4,17 +4,22 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 
-public class Main {
+public class rain {
     public static void main(String[] args) {
         try {
+            private String rain_url = (
+                "https://api.meteomatics.com/" +
+                yesterday_time +
+                today_time +
+                "/precip_24h:mm/" +
+                lattitude +
+                "," +
+                longitude +
+                "/json?api_key=" +
+                rain_api_key()
+                )
 
-            URL url = new URL("https://api.meteomatics.com/
-            yesterday
-            --today at midnight
-            :PT5M/precip_24h:mm/
-            latitude,
-            longitude
-            /json?api_key=DEMO_KEY");//your url i.e fetch data from . //dont know how to put in username and password
+            URL url = new URL(rain_url)
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setRequestMethod("GET");
             conn.setRequestProperty("Accept", "application/json");
@@ -22,12 +27,7 @@ public class Main {
                 throw new RuntimeException("Failed : HTTP Error code : "
                         + conn.getResponseCode());
             }
-            InputStreamReader in = new InputStreamReader(conn.getInputStream());
-            BufferedReader br = new BufferedReader(in);
-            String output;
-            while ((output = br.readLine()) != null) {
-                System.out.println(output);
-            }
+            
             conn.disconnect();
 
         } catch (Exception e) {
