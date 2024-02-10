474fcbe93320134d7c9bb5a2f952498babf2317b
itsasam1 <mikeeaglesam@gmail.com>
2024-02-10 01:48:30 -0600

    Updated weekly_rain java file to include variable still need to add json parsing

--- api/weekly_rain.java
+++ api/weekly_rain.java
@@ -8,7 +8,13 @@ public class Main {
     public static void main(String[] args) {
         try {
 
-            URL url = new URL("https://api.meteomatics.com/2024-02-10T00:00:00Z--2024-02-13T00:00:00Z:PT5M/precip_5min:mm/47.412164,9.340652/json?api_key=DEMO_KEY");//your url i.e fetch data from .
+            URL url = new URL("https://api.meteomatics.com/
+            yesterday
+            --today at midnight
+            :PT5M/precip_24h:mm/
+            latitude,
+            longitude
+            /json?api_key=DEMO_KEY");//your url i.e fetch data from . //dont know how to put in username and password
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setRequestMethod("GET");
             conn.setRequestProperty("Accept", "application/json");
