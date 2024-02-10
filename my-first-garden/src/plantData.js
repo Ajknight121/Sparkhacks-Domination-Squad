3e13f17cdc9cf6f1a79c64e2839cce6a17c852db
itsasam1 <mikeeaglesam@gmail.com>
2024-02-10 12:26:50 -0600

    added the northern list

--- my-first-garden/src/plantData.js
+++ my-first-garden/src/plantData.js
@@ -1,17 +1,34 @@
 export const NorthernIllinoisplants = [
     {
-      plantName: "Beets",
-      zones:
-      info:["takes up 3ft", "requires 1gal of water per day"],
-      pests: ["caterpillers", "Rodents"],
-    },
-    {
         plantName: "Beets",
-        zones: [2-11],
-        dates: [4/24,7/30],
+        zones: ["2-11"],
+        dates: ["4/24","7/30"],
         size:[1],
-        water:["soak befor planting"],
+        info:["Soak in water 24 hours before planting","Beets are not the best at competing with weeds especially when younger, regular weeding and mulching when the leaves get tall enough is recommended"],
         sun:[0],
-        pests: ["regular weeding required"],
+    }
+    {
+        plantName: "Turnip",
+        zones: ["2-9"],
+        dates: ["5/15","8/5"],
+        size:[2],
+        sun:[0],
+        info: ["Root maggots are the most prevalent problem, use diatomaceous earth around the root to dehydrate and kill them"],
+    }
+    {
+        plantName: "Peas",
+        zones: ["2-11"],
+        dates: ["4/24","5/15"],
+        size:[1],
+        sun:[0],
+        info: ["Root-rot disease is the most prevalent problem with peas, which can be resolved with properly drained soil."],
+    }
+    {
+        plantName: "Winter Squash",
+        zones: ["3-9"],
+        dates: ["6/5","6/15"],
+        size:[1],
+        sun:[0],
+        info: ["The biggest problem is cucumber beetles, they can be deterred with neem oil extract, a natural pesticide against them."],
     }
   ]
